26bccae038b8c4725741425f7499e8c718263f4c
Clive Sasaka <clivesasaka@gmail.com>
2023-09-14 17:03:05 +0300

    Add a route that deals with registration of users

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -14,6 +14,18 @@ def index() -> str:
     '''Method that returns a message for the index page'''
     return jsonify({"message": "Bienvenue"})
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    '''Method that registers a user'''
+    try:
+        email = request.form['email']
+        password = request.form['password']
+        new_user = AUTH.register_user(email, password)
+        return jsonify({"email": new_user.email,
+                        "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
