6f90e66c486e363134a03e860f35dde3a117ba8c
Clive Sasaka <clivesasaka@gmail.com>
2023-09-14 17:43:46 +0300

    Final authorization and apllication modules

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -14,6 +14,7 @@ def index() -> str:
     '''Method that returns a message for the index page'''
     return jsonify({"message": "Bienvenue"})
 
+
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def users() -> str:
     '''Method that registers a user'''
@@ -25,6 +26,67 @@ def users() -> str:
                         "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    '''Method that creates a new session for the user'''
+    email = request.form['email']
+    password = request.form['password']
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout() -> None:
+    '''Method that deletes the user session / logout'''
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
+        abort(403)
+
+
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile() -> str:
+    '''Method that finds the user corresponding to the session ID'''
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+    else:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    '''Method that generates a reset token'''
+    try:
+        email = request.form['email']
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    '''Method that updates the password'''
+    try:
+        email = request.form['email']
+        reset_token = request.form['reset_token']
+        new_password = request.form['new_password']
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
 
 
 if __name__ == "__main__":
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,14 +5,17 @@ from db import DB
 from user import User
 from uuid import uuid4
 
+
 def _hash_password(password: str) -> str:
     '''Method that takes in a password string arguments and returns a
     bytes'''
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
+
 def _generate_uuid() -> str:
     '''Method that returns a string representation of a new UUID'''
     return str(uuid4())
+
 
 class Auth:
     """Auth class to interact with the authentication database.
@@ -20,7 +23,7 @@ class Auth:
 
     def __init__(self):
         self._db = DB()
-        
+
     def register_user(self, email: str, password: str) -> User:
         '''Method that takes mandatory email and password string arguments
         and returns a User object stored in the database'''
@@ -28,6 +31,63 @@ class Auth:
             raise ValueError('User {} already exists'.format(email))
         else:
             return self._db.add_user(email, _hash_password(password))
-    
-        
-        
+
+    def valid_login(self, email: str, password: str) -> bool:
+        '''Method that takes email and password string arguments and
+        returns a boolean'''
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
+        except Exception:
+            return False
+
+    def create_session(self, email: str) -> str:
+        '''Method that takes an email string argument and returns the session
+        ID as a string'''
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except Exception:
+            return None
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        '''Method that takes a single session_id string argument and
+        returns the corresponding User or None'''
+        if session_id is None:
+            return None
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except Exception:
+            return None
+
+    def destroy_session(self, user_id: int) -> None:
+        '''Method that takes a single user_id integer argument and returns
+        None'''
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except Exception:
+            return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        '''Method that takes an email string argument and returns a string'''
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except Exception:
+            raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        '''Method that takes reset_token string argument and a password
+        string argument and returns None'''
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_password,
+                                 reset_token=None)
+        except Exception:
+            raise ValueError
