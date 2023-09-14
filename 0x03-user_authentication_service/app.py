159f65b833010f472acd70f72d932996dd3a28e2
Clive Sasaka <clivesasaka@gmail.com>
2023-09-14 16:57:49 +0300

    Fizx the last aprt of the app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''This is the app module'''
 from flask import Flask, jsonify, request, abort, redirect
+
 from auth import Auth
 
 
@@ -12,3 +13,7 @@ AUTH = Auth()
 def index() -> str:
     '''Method that returns a message for the index page'''
     return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
