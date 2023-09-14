33a2297d00a1b8dd57a9a7f504c93064e673edd9
Clive Sasaka <clivesasaka@gmail.com>
2023-09-14 12:12:45 +0300

    Fix  the add_user method in the db class

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,6 +34,10 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         '''Method that saves a new user instance to the database'''
         new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
+        try:
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            new_user = None
         return new_user
